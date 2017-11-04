5a675d5062f5304e0e4def2a35d2c1bd05bb9be4
Kang Min Ju <kmju1997@naver.com>
2017-11-04 14:49:16 +0900

    Update run.py

--- file/run.py
+++ file/run.py
@@ -3,5 +3,5 @@ import json
 
 postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
-response.write("hello world from "+postreqdata['name'])
+response.write("hello worlds from "+postreqdata['name'])
 response.close()
